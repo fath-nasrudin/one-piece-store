1a8c25b89d5847a49e27b7cdc41098dff34d8f34
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-13 20:30:26 +0700

    change typing style

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,22 +1,23 @@
 const router = require('express').Router();
 const ProductController = require('../controllers/product.controller');
 
-router.route('/')
-  .get(ProductController.getProductList);
+router.route('/').get(ProductController.getProductList);
 
-router.route('/:id')
-  .get(ProductController.getProductDetail);
+router.route('/:id').get(ProductController.getProductDetail);
 
-router.route('/:id/create')
+router
+  .route('/:id/create')
   .get(ProductController.getAddProduct)
   .post(ProductController.postAddProduct);
 
-router.route('/:id/update')
+router
+  .route('/:id/update')
   .get(ProductController.getUpdateProduct)
   .post(ProductController.postUpdateProduct);
 
-router.route('/:id/delete')
+router
+  .route('/:id/delete')
   .get(ProductController.getDeleteProduct)
   .post(ProductController.postDeleteProduct);
-  
+
 module.exports = router;
