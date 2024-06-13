46b141c23b04e80a32832fb7a81c9a16d9d9475e
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-13 21:52:20 +0700

    Add generic validators handler

---
+++ middlewares/validatorHandler.js
@@ -0,0 +1,29 @@
+const { body, validationResult } = require('express-validator');
+
+module.exports.validationResult = validationResult;
+
+module.exports.validateName = (bodyName = 'name', options = {}) => {
+  let {min, max} = options;
+  if (min === undefined) min = 3;
+  if (max === undefined) max = 30;
+
+  return body(bodyName)
+    .trim()
+    .escape()
+    .isString().withMessage(`${bodyName} must be a string`)
+    .isLength({min, max}).withMessage(`${bodyName} must be between ${min} and ${max} characters`);
+};
+
+module.exports.validateNumber = (bodyName = 'number') => {
+  return body(bodyName)
+    .trim()
+    .escape()
+    .isNumeric().withMessage(`${bodyName} must be a number`)
+};
+
+module.exports.validateMongoId = (bodyName = '_id') => {
+  return body(bodyName)
+    .trim()
+    .escape()
+    .isMongoId().withMessage(`${bodyName} must be a valid ObjectId`)
+};
