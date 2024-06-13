a6cb9ea8b6e654285f50c2fbdf6347fc5c1cd4d3
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-13 22:41:29 +0700

    Add delete category prehook to delete product's ref category

--- models/category.js
+++ models/category.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
 
 const Schema = mongoose.Schema;
 
@@ -8,6 +9,15 @@ const categorySchema = new Schema({
 
 categorySchema.virtual('url').get(function() {
   return `/categories/${this._id}`;
+})
+
+categorySchema.pre('findOneAndDelete', async function(next) { 
+  const categoryId = this.getQuery()['_id'];
+  await Product.updateMany(
+    { categories: categoryId },
+    { $pull: { categories: categoryId }}
+  )
+  next();
 })
 
 module.exports = mongoose.model('Category', categorySchema);
