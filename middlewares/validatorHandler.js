41270dc1c198e3fc2cf7c2cff34dff23191cd7b4
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-14 01:40:27 +0700

    Add middleware validator - string validator

--- middlewares/validatorHandler.js
+++ middlewares/validatorHandler.js
@@ -2,6 +2,18 @@ const { body, validationResult } = require('express-validator');
 
 module.exports.validationResult = validationResult;
 
+module.exports.validateString = (bodyName, options = {}) => {
+  let {min, max} = options;
+  if (min === undefined) min = 0;
+  if (max === undefined) max = 100;
+
+  return body(bodyName)
+    .trim()
+    .escape()
+    .isString().withMessage(`${bodyName} must be a string`)
+    .isLength({min, max}).withMessage(`${bodyName} must be between ${min} and ${max} characters`);
+};
+
 module.exports.validateName = (bodyName = 'name', options = {}) => {
   let {min, max} = options;
   if (min === undefined) min = 3;
